4f91802a22b1fd200725bfa1f7318c38548bcb7b
Kaushik Reddy <kaushik.reddy@hotmail.com>
2018-07-23 10:16:42 -0600

    Added guest user for current_user

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -33,7 +33,7 @@ class PortfoliosController < ApplicationController
     @portfolio_item = Portfolio.find(params[:id])
       
     respond_to do |format|
-    	if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+    	if @portfolio_item.update(portfolio_params)
      		format.html { redirect_to portfolios_path, notice: 'The record was successfully updated.' }
     	else
       	format.html { render :edit }
@@ -58,7 +58,11 @@ class PortfoliosController < ApplicationController
   private
 
   def portfolio_params
-    params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name])
+    params.require(:portfolio).permit(:title, 
+                                      :subtitle, 
+                                      :body, 
+                                      technologies_attributes: [:name]
+                                     )
   end
 
 end
